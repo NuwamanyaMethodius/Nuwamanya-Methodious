0e1c52baa34d2e73d509c58a387de5a594271826
NuwamanyaMethodius <2023abit1924f@kab.ac.ug>
2024-09-20 04:08:19 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="NUWAMANYA METHODIUS";
+$DOB="02/11/1999";
+$Address="MBARARA";
+$CYear=2024;
+$BYear=1999;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+CYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
